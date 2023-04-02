4accabf9e09617d6fb66c3acda6c9eaff6c285c5
hauensas <hauensas@students.zhaw.ch>
2023-04-02 18:15:36 +0200

    update

--- src/websiteChanges.py
+++ src/websiteChanges.py
@@ -4,50 +4,68 @@ import hashlib
 from urllib.request import urlopen, Request
 from releasenoteScraper import *
 import logging
+import sys
+import urllib.error
 
 # create logger
 logging.basicConfig(filename='website_changes.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.info("script started")
 
-# setting the URL you want to monitor
-url = Request('https://www.20min.ch/',
-			headers={'User-Agent': 'Mozilla/5.0'})
+try:
+    # setting the URL you want to monitor
+    logging.info("reading defined url")
+    url = Request('https://www.20min.ch', headers={'User-Agent': 'Mozilla/5.0'})
 
-# to perform a GET request and load thegi
-# content of the website and store it in a var
-response = urlopen(url).read()
+    # to perform a GET request and load the content of the website and store it in a var
+    logging.info("GET request to url")
+    response = urlopen(url).read()
+    
+except urllib.error.URLError as e:
+    logging.error("Failed to open URL")
+    #logging.exception('')
+    logging.error("exit the script")
+    sys.exit(1) # Exit the script with a non-zero exit code
+    
+    
+except Exception as e:
+    logging.error("An error occurred")
+    #logging.exception('')
+    logging.error("exit the script")
+    sys.exit(1) # Exit the script with a non-zero exit code
+    
 
 # to create the initial hash
+logging.info("create initial hash")
 currentHash = hashlib.sha224(response).hexdigest()
-logging.info("Website monitor started")
 print("running")
+logging.info("wait 10 seconds")
 time.sleep(10)
 while True:
 	try:
 		# perform the get request and store it in a var
+		logging.info("GET request to url")
 		response = urlopen(url).read()
-		logging.info("reading url")
 
 		# create a hash
-		currentHash = hashlib.sha224(response).hexdigest()
-		logging.info("create current hash")
+		logging.info("create hash")
+		currentHash = hashlib.sha224(response).hexdigest()		
 
 		# wait for 30 seconds
-		time.sleep(30)
 		logging.info("wait 3o seconds")
+		time.sleep(30)		
 
 		# perform the get request
+		logging.info("GET request to url")
 		response = urlopen(url).read()
-		logging.info("reading url")
-
+		
 		# create a new hash
-		newHash = hashlib.sha224(response).hexdigest()
 		logging.info("create new hash")
+		newHash = hashlib.sha224(response).hexdigest()		
 
 		# check if new hash is same as the previous hash
 		if newHash == currentHash:
 			logging.info("compare hashes: no changes")
-			continue	
-			
+			continue				
 
 		# if something changed in the hashes
 		else:
@@ -57,20 +75,29 @@ while True:
 			send_email()
 
 			# again read the website
-			response = urlopen(url).read()
-			logging.info("reading url")
+			logging.info("GET request to url")
+			response = urlopen(url).read()			
 
 			# create a hash
-			currentHash = hashlib.sha224(response).hexdigest()
 			logging.info("create current hash")
+			currentHash = hashlib.sha224(response).hexdigest()			
 
 			# wait for 30 seconds
-			time.sleep(30)
 			logging.info("wait 3o seconds")
+			time.sleep(30)			
 			continue
 
 	# To handle exceptions
+	except urllib.error.URLError as e:
+		logging.error("Failed to open URL")
+		#logging.exception('')
+		logging.error("exit the script")
+		sys.exit(1) # Exit the script with a non-zero exit code
+		
+    
 	except Exception as e:
-		logging.error("Error occurred")
-		logging.exception('')
-		print("error")
+		logging.error("An error occurred")
+		#logging.exception('')
+		logging.error("exit the script")
+		sys.exit(1) # Exit the script with a non-zero exit code
+		
